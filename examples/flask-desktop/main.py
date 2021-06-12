680d5500c5a71b022eab1a19e0e1171cc59fce17
alincmt <aclimente@ymail.com>
2021-06-12 13:48:44 +0300

    Add files via upload

--- examples/flask-desktop/main.py
+++ examples/flask-desktop/main.py
@@ -14,6 +14,14 @@ def hello():
 def home(): 
     return render_template('some_page.html')
 
+
+@app.route("/keep-server-alive", methods=['GET'])
+def keep_alive():
+    FlaskUI.keep_server_running()
+    return ""
+
+
+
 if __name__ == "__main__":
     ui.run()
    
