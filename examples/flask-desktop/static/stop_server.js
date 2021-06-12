58a1b1567362c712751d8ac4f67407d1ca4563d1
alincmt <aclimente@ymail.com>
2021-06-12 13:47:30 +0300

    Add files via upload

---
+++ examples/flask-desktop/static/stop_server.js
@@ -0,0 +1,24 @@
+
+async function getRequest(url='') {
+    const response = await fetch(url, {
+      method: 'GET', 
+      cache: 'no-cache'
+    })
+    return response.json()
+}
+  
+document.addEventListener('DOMContentLoaded', function() {
+
+let url = document.location
+let route = "/keep-server-alive"
+let interval_request = 3 * 1000 //sec
+
+function keep_alive_server(){
+    getRequest(url + route)
+    .then(data => console.log(data))
+}
+
+setInterval(keep_alive_server, interval_request)()
+
+})
+
