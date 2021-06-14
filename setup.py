b5218c2f48fef25c91124cce6a13339859b0316a
ClimenteA <climente.alin@gmail.com>
2021-06-14 17:18:21 +0300

    removed psutil dependency, added middleware and js code instead of unpredictable browser PID, refactor

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import sys
 from setuptools import setup, find_packages
 
 #Distribute py wheels
@@ -16,7 +17,13 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
-setup (
+REQUIREMENTS = []
+if sys.platform.startswith('linux'):
+	REQUIREMENTS = ['whichcraft']
+    
+
+
+setup(
 	name="flaskwebgui",
 	version="0.3.0",
 	description="Create desktop applications with Flask/Django/FastAPI!",
@@ -25,7 +32,7 @@ setup (
 	author_email="climente.alin@gmail.com",
 	license='MIT',
 	py_modules=["flaskwebgui"],
-	install_requires=['whichcraft', 'waitress'],
+	install_requires=REQUIREMENTS,
 	packages=find_packages(),
 	long_description=long_description,
     long_description_content_type="text/markdown",
