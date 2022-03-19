cc864d55cba4ab39c3f67c934d674e65b9881c6c
ClimenteA <climente.alin@gmail.com>
2022-03-19 10:36:05 +0200

    tested pull requests, fixed small issue

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ if sys.platform.startswith('linux'):
 
 setup(
 	name="flaskwebgui",
-	version="0.3.4",
+	version="0.3.5",
 	description="Create desktop applications with Flask/Django/FastAPI!",
 	url="https://github.com/ClimenteA/flaskwebgui",
 	author="Climente Alin",
