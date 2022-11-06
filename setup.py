d81ba8d8412095dd54d28757ac85ed9291165821
ClimenteA <climente.alin@gmail.com>
2022-11-06 15:08:43 +0200

    feat: updated setup.py

--- setup.py
+++ setup.py
@@ -1,40 +1,26 @@
-import sys
 from setuptools import setup, find_packages
 
-#Distribute py wheels
-#python3 setup.py bdist_wheel sdist
-#twine check dist/*
-#cd dist 
-#twine upload *
-
-#Basic GIT
-#git init/clone
-#git add --all
-#git commit -m "made some changes"
-#git push origin master
+# Distribute py wheels
+# python3 setup.py bdist_wheel sdist
+# twine check dist/*
+# cd dist
+# twine upload *
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-
-REQUIREMENTS = []
-if sys.platform.startswith('linux'):
-	REQUIREMENTS = ['whichcraft']
-    
-
-
 setup(
-	name="flaskwebgui",
-	version="0.3.7",
-	description="Create desktop applications with Flask/Django/FastAPI!",
-	url="https://github.com/ClimenteA/flaskwebgui",
-	author="Climente Alin",
-	author_email="climente.alin@gmail.com",
-	license='MIT',
-	py_modules=["flaskwebgui"],
-	install_requires=REQUIREMENTS,
-	packages=find_packages(),
-	long_description=long_description,
+    name="flaskwebgui",
+    version="1.0.0",
+    description="Create desktop applications with Flask/Django/FastAPI!",
+    url="https://github.com/ClimenteA/flaskwebgui",
+    author="Climente Alin",
+    author_email="climente.alin@gmail.com",
+    license="MIT",
+    py_modules=["flaskwebgui"],
+    # install_requires=REQUIREMENTS,
+    packages=find_packages(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
-	package_dir={"":"src"}
+    package_dir={"": "src"},
 )
