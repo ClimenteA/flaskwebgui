a07cec452851050e8145b566d7b0d59ea2413da9
ClimenteA <climente.alin@gmail.com>
2022-11-12 14:28:49 +0200

    chore: updated version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="flaskwebgui",
-    version="1.0.0",
+    version="1.0.1",
     description="Create desktop applications with Flask/Django/FastAPI!",
     url="https://github.com/ClimenteA/flaskwebgui",
     author="Climente Alin",
