a6181dc0d2f3e47ac01330b39f32f29a799cb365
Elsevar Asadov <101859185+Elsexver@users.noreply.github.com>
2023-02-18 06:43:12 +0400

    Update flaskwebgui.py

--- src/flaskwebgui.py
+++ src/flaskwebgui.py
@@ -74,9 +74,9 @@ def find_browser_on_mac():
 
 def find_browser_on_windows():
     paths = [
-        "C:\Program Files\Google\Chrome\Application\chrome.exe",
         "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe",
         "C:\Program Files\Microsoft\Edge\Application\msedge.exe",
+        "C:\Program Files\Google\Chrome\Application\chrome.exe",
         "C:\Program Files\BraveSoftware\Brave-Browser\Application\\brave.exe",
     ]
     for path in paths:
